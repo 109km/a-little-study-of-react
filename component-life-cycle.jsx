7256b17fab8b6f521eef3a5d751203b77ebdba28
xinheng.shao <shaoxinheng@gmail.com>
2015-11-06 10:34:37 +0800

    add files

---
+++ component-life-cycle.jsx
@@ -0,0 +1,14 @@
+var App = React.createClass({
+    render:function(){
+        return (
+            <div>
+                Hello,
+                <h2>world</h2>
+            </div>
+        );
+    }
+});
+ReactDOM.render(
+    <App />,
+    document.body
+);
